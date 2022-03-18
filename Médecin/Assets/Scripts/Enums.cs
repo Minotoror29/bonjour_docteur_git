cb59f15a3b8194822eaa21a9d2055b9e1f1ad2fe
Minotoror29 <matthieu.boissel@laposte.net>
2022-03-18 17:22:28 +0100

    Revert "Merge branch 'main' of https://github.com/Minotoror29/bonjour_docteur_git"
    This reverts commit 93a1ec4950702b76a3a5e6ff808087fb519d6722, reversing
    changes made to 25e2e09b20b10510b8c62d84b696eb2579f91c3e.

--- Médecin/Assets/Scripts/Enums.cs
+++ Médecin/Assets/Scripts/Enums.cs
@@ -6,7 +6,7 @@ public enum VILLAGEOIS { Personne, Sylvie, René, Marguerite }
 
 public enum MALADIES { Aucune, Angine, Appendicite, Cancer, Cholestérol, Gastro, Grippe, Hypertension, Laryngite, Plaie, Rage, Rougeole, Ulcère}
 
-public enum TRAITEMENTS { Aucun, Faber, Flesemine, Alatusium, Dosilide, Rabigux, Luparast, Brasitam, Cholefix, Sprilyme, Trimoglucémol, Biovotoc }
+public enum TRAITEMENTS { Aucun, Syrop, Flesemine, Alatusium, Dosilide, Antirabique, Luparast, Brasitam, Choléfix, Antiseptique, Trimoglucémol, Antibiotique }
 
 public enum SYMPTOMES { Aucun, Placebo, Fièvre, Douleur, Nausée, Diarhée, Rage, Toux, Inflammations, Cholestérol, Infections, LésionsGastriques, Bactéries, Microbes, Spécialiste}
 
