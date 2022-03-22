ec8c3208502236557af9c9569cf05a9c751e0f65
Minotoror29 <matthieu.boissel@laposte.net>
2022-03-22 16:27:26 +0100

    lignes

--- Médecin/Assets/Scripts/Dialogues/DialogueLigne.cs
+++ Médecin/Assets/Scripts/Dialogues/DialogueLigne.cs
@@ -19,11 +19,11 @@ public class DialogueLigne : MonoBehaviour
         // Détermine qui parle et ajuste le texte en fonction
         if (type == TypeLigne.Patient)
         {
-            GetComponent<Text>().alignment = TextAnchor.UpperLeft;
+            GetComponent<Text>().alignment = TextAnchor.UpperRight;
             GetComponent<Text>().color = new Color(0.5490196f, 0.8588236f, 0.8705883f);
         } else if (type == TypeLigne.Médecin)
         {
-            GetComponent<Text>().alignment = TextAnchor.UpperRight;
+            GetComponent<Text>().alignment = TextAnchor.UpperLeft;
             GetComponent<Text>().color = new Color(1f, 0.8274511f, 0.5803922f);
         }
     }
